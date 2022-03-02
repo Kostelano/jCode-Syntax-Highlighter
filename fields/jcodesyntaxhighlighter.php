f582f1059aff171cd6cc31e2aec661006aff9c8f
Konstantin Kolos <k0nstantinkolos88@gmail.com>
2022-03-02 22:38:00 +0200

    Copyrights and versioning

--- fields/jcodesyntaxhighlighter.php
+++ fields/jcodesyntaxhighlighter.php
@@ -3,7 +3,7 @@
  * @package	Joomla.Platform
  * @subpackage	Content.jCodeSyntaxHighlighter
  *
- * @copyright	Copyright (C) 2005 - 2021 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright	Copyright (C) 2005 - 2022 Open Source Matters, Inc. <https://www.joomla.org>
  * @license	GNU General Public License version 2 or later; see LICENSE.txt
  */
 
